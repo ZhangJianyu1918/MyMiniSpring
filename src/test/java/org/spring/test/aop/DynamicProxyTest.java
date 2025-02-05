c602a6021a194fa0628a285c08f6ba17ff7752d0
ZhangJianyu <jzha0488@student.monash.edu>
2025-02-05 16:14:29 +1100

    add cglib proxy

--- src/test/java/org/spring/test/aop/DynamicProxyTest.java
+++ src/test/java/org/spring/test/aop/DynamicProxyTest.java
@@ -1,10 +1,12 @@
 package org.spring.test.aop;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 import org.spring.aop.AdvisedSupport;
 import org.spring.aop.MethodMatcher;
 import org.spring.aop.TargetSource;
 import org.spring.aop.aspectj.AspectJExpressionPointcut;
+import org.spring.aop.framework.CglibAopProxy;
 import org.spring.aop.framework.JdkDynamicAopProxy;
 import org.spring.test.common.WorldServiceInterceptor;
 import org.spring.test.service.WorldService;
@@ -12,20 +14,33 @@ import org.spring.test.service.WorldServiceImpl;
 
 public class DynamicProxyTest {
 
-    @Test
-    public void testJdkDynamicProxy() throws Exception {
+    private AdvisedSupport advisedSupport;
+
+    @Before
+    public void setup() {
         WorldService worldService = new WorldServiceImpl();
 
-        AdvisedSupport advisedSupport = new AdvisedSupport();
+        advisedSupport = new AdvisedSupport();
         TargetSource targetSource = new TargetSource(worldService);
         WorldServiceInterceptor worldServiceInterceptor = new WorldServiceInterceptor();
         MethodMatcher methodMatcher = new AspectJExpressionPointcut("execution(* org.spring.test.service.WorldService.explode(..))").getMethodMatcher();
         advisedSupport.setTargetSource(targetSource);
         advisedSupport.setMethodInterceptor(worldServiceInterceptor);
         advisedSupport.setMethodMatcher(methodMatcher);
+    }
 
+
+    @Test
+    public void testJdkDynamicProxy() throws Exception {
         WorldService proxy = (WorldService) new JdkDynamicAopProxy(advisedSupport).getProxy();
         proxy.explode();
 
     }
+
+    @Test
+    public void testCglibDynamicProxy() throws Exception {
+        WorldService proxy = (WorldService) new CglibAopProxy(advisedSupport).getProxy();
+        proxy.explode();
+    }
+
 }
